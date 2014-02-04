4ac2dfb1bab2457e93baba631e1f450b3193fcf6
Igor Janošev <igor.janosev@gmail.com>
2014-02-04 16:57:21 +0100

    Reformated some code

--- src/slagalica/core/Dictionary.java
+++ src/slagalica/core/Dictionary.java
@@ -9,45 +9,44 @@ import java.util.List;
 
 public class Dictionary {
 
-	private List<String>	dictionary	= new ArrayList<String>();
+    private List<String> dictionary = new ArrayList<String>();
 
-	public Dictionary(String dictionaryPath) throws FileNotFoundException {
-		String inputLine;
-		BufferedReader inputFile;
-		try {
-			inputFile = new BufferedReader(new InputStreamReader(Thread.currentThread().getContextClassLoader()
-					.getResourceAsStream(dictionaryPath), "utf-8"));
-			while ((inputLine = inputFile.readLine()) != null) {
-				dictionary.add(inputLine);
-			}
-			inputFile.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+    public Dictionary(String dictionaryPath) throws FileNotFoundException {
+        String inputLine;
+        BufferedReader inputFile;
+        try {
+            inputFile = new BufferedReader(new InputStreamReader(Thread.currentThread().getContextClassLoader().getResourceAsStream(dictionaryPath), "utf-8"));
+            while ((inputLine = inputFile.readLine()) != null) {
+                dictionary.add(inputLine);
+            }
+            inputFile.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-	static public boolean containsWord(String word, String letters) {
-		ArrayList<Character> localLetters = new ArrayList<Character>();
-		for (char tempChar : letters.toLowerCase().toCharArray()) {
-			localLetters.add(tempChar);
-		}
-		for (char tempChar : word.toCharArray()) {
-			if (!localLetters.remove(Character.valueOf(tempChar))) {
-				return false;
-			}
+    static public boolean containsWord(String word, String letters) {
+        ArrayList<Character> localLetters = new ArrayList<Character>();
+        for (char tempChar : letters.toLowerCase().toCharArray()) {
+            localLetters.add(tempChar);
+        }
+        for (char tempChar : word.toCharArray()) {
+            if (!localLetters.remove(Character.valueOf(tempChar))) {
+                return false;
+            }
 
-		}
-		return true;
-	}
+        }
+        return true;
+    }
 
-	public List<String> findAllWords(String letters) {
-		ArrayList<String> localList = new ArrayList<String>();
-		for (String word : dictionary) {
-			if (letters == null || containsWord(word, letters)) {
-				localList.add(word);
-			}
-		}
-		return localList;
-	}
+    public List<String> findAllWords(String letters) {
+        ArrayList<String> localList = new ArrayList<String>();
+        for (String word : dictionary) {
+            if (letters == null || containsWord(word, letters)) {
+                localList.add(word);
+            }
+        }
+        return localList;
+    }
 
 }
--- src/slagalica/gui/FindWordActionListener.java
+++ src/slagalica/gui/FindWordActionListener.java
@@ -5,16 +5,16 @@ import java.awt.event.ActionListener;
 
 public class FindWordActionListener implements ActionListener {
 
-	private SlagalicaGUI	owner;
+    private SlagalicaGUI owner;
 
-	public FindWordActionListener(SlagalicaGUI owner) {
-		super();
-		this.owner = owner;
-	}
+    public FindWordActionListener(SlagalicaGUI owner) {
+        super();
+        this.owner = owner;
+    }
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		owner.updateTheList();
-	}
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        owner.updateTheList();
+    }
 
 }
